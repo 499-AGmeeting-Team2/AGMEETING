4272de1bf0096ede07161095ade82a3c97511f29
Karan Gohil <karandilipsinhgohil@gmail.com>
2022-02-11 21:06:40 -0800

    added server connection

---
+++ server/participant/users.js
@@ -0,0 +1,41 @@
+const { Sequelize, DataTypes } = requires("sequelize");
+const database = require("../configuration/seq");
+
+const users = database.define(
+  "users",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    client_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    registered_data: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    company_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
+
+console.log(users == database.models.users);
+
+module.exports = users;
--- server/server.js
+++ server/server.js
@@ -4,30 +4,42 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 3000;
 // import routes from "../client/routes";
-const indexRoutes = require("./controllers/index.controller")
+const indexRoutes = require("./controllers/index.controller");
 const DIST_DIR = path.join(__dirname, "public");
 const HTML_FILE = path.join(DIST_DIR, "index.html");
-
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.static(path.join(__dirname, "../dist")));
 
+//db connection
+const { Sequelize } = require("sequelize");
+const db = new Sequelize("postgres://postgres:admin@localhost:5432/postgres");
+
+// Test DB
+db.authenticate()
+  .then(() => console.log("Database connected .."))
+  .catch((err) => console.log("Error: " + err));
+/*try {
+db.authenticate();
+console.log("Connection has been established successfully.");
+} catch (error) {
+console.error("Unable to connect to the database:", error);
+} */
 
 // app.use(express.static("helper"));
 // app.use("/", indexRoutes)
 app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 
-    res.sendFile(path.join(__dirname, '../dist', 'index.html'), function (err) {
-        if (err) {
-            res.status(500).send(err);
-        }
-    });
-
-    // do not use this
-    // res.sendFile(path.join(__dirname + '/public/index.html'))
+  // do not use this
+  // res.sendFile(path.join(__dirname + '/public/index.html'))
 });
 
 app.listen(port, () => {
-    console.log(`The app server is running on port: ${port}`);
+  console.log(`The app server is running on port: ${port}`);
 });
