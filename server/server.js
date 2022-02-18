cce1f1772f26e96261740549291ece240d2ec1d6
Karan Gohil <karandilipsinhgohil@gmail.com>
2022-02-18 00:31:31 -0800

    working migration

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express = require("express");
 // var fallback = require('express-history-api-fallback')
 const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 // import routes from "../client/routes";
-const indexRoutes = require("./controllers/index.controller");
 const DIST_DIR = path.join(__dirname, "public");
 const HTML_FILE = path.join(DIST_DIR, "index.html");
 
@@ -12,8 +13,13 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.static(path.join(__dirname, "../dist")));
 
+// Bodyparser middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Database
 const db = require("./config/db");
+const { models } = require("./config/db");
 
 // Test DB
 db.authenticate()
@@ -25,9 +31,20 @@ console.log("Connection has been established successfully.");
 } catch (error) {
 console.error("Unable to connect to the database:", error);
 } */
-
 // gets the model and the required route/controller
-app.use("/users", require("./controllers/users.controller"));
+//app.use("/users", require("./controllers/users.controller"));
+
+// we have to only get index.js and it will import all the models
+const allmodels = require("./models/index");
+
+//gets the data from signin page
+app.post("/signin", (req) => {
+  console.log(req.body);
+  allmodels.User.create({
+    email: req.body.email,
+    password: req.body.password,
+  });
+});
 
 // app.use(express.static("helper"));
 // app.use("/", indexRoutes)
